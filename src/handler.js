b7c39afa30433f2c71350b8c31e27314028d7314
Akhtarbilal97 <80459974+Akhtarbilal97@users.noreply.github.com>
2023-05-30 13:38:08 +0500

    complete upto integration of category

--- src/handler.js
+++ src/handler.js
@@ -185,126 +185,446 @@ module.exports.signIn = async (event, context) => {
 };
 
 
+// const { v4: uuidv4 } = require("uuid");
+//   // const AWS = require("aws-sdk");
+// const s3 = new AWS.S3();
+// const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
-const { v4: uuidv4 } = require("uuid");
-const s3 = new AWS.S3();
-const dynamoDB = new AWS.DynamoDB.DocumentClient(); // Create a DynamoDB instance
-
-module.exports.uploadFile = async (event, context) => {
-  const fileContent = Buffer.from(event.body, "base64");
-  const fileName = event.queryStringParameters.filename;
-  const bucketName ="my-serverless-app-dev-serverlessdeploymentbucket-ew2x60uk92zg";
-  const fileType = fileName.split(".")[1];
-
-  const params = {
-    Bucket: bucketName,
-    Key: fileName,
-    Body: fileContent,
-    ContentType: `application/${fileType}`,
-    ACL: "public-read",
-  };
-
-  try {
-    const result = await s3.upload(params).promise();
-    console.log(`File uploaded successfully to ${result.Location}`);
-
-    // Store user data in DynamoDB
-    const fileData = {
-      id: uuidv4(),
-      title: "My file title",
-      description: "A brief description of my file",
-      type: fileType,
-      category_id: null,
-      fileType: null,
-      previewUrl: null,
-      fileUrl: `https://${bucketName}.s3.amazonaws.com/${fileName}`,
-      isPremium: false,
-      likesCount: 0,
-      viewsCount: 0,
-    };
-    const dynamoDBParams = {
-      TableName: "assets-table",
-      Item: fileData,
-    };
-    await dynamoDB.put(dynamoDBParams).promise();
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "File uploaded successfully" }),
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: "Failed to upload file" }),  
-    };
-  }
-};
-
-
-// module.exports.SearchService = async (event) => {
+// // // upload file api
+// module.exports.uploadFile = async (event, context) => {
 //   try {
-//     console.log("Received event:", JSON.stringify(event));
+//     if (!event.body) {
+//       throw new Error("Missing file content");
+//     }
 
-//     const { queryStringParameters } = event;
-//     const { title } = queryStringParameters;
+//     const fileContent = Buffer.from(event.body, "base64");
+//     const fileName = event.queryStringParameters.filename || "defaultFilename";
+//     const bucketName =
+//       "my-serverless-app-dev-serverlessdeploymentbucket-ew2x60uk92zg";
+//     const fileType = fileName.split(".")[1];
 
-//     console.log("title:", title);
-
-//     const searchParams = {
-//       TableName: "assets-table",
-//       FilterExpression: "contains(#title, :title)",
-//       ExpressionAttributeNames: {
-//         "#title": "title",
-//       },
-//       ExpressionAttributeValues: {
-//         ":title": title,
-//       },
+//     const params = {
+//       Bucket: bucketName,
+//       Key: fileName,
+//       Body: fileContent,
+//       ContentType: `application/${fileType}`,
+//       ACL: "public-read",
 //     };
 
-//     const searchResults = await dynamoDB.scan(searchParams).promise();
+//     const result = await s3.upload(params).promise();
+//     console.log(`File uploaded successfully to ${result.Location}`);
+
+//     // Parse the metadata from the request body
+//     const metadata = JSON.parse(event.body);
+
+//     const fileData = {
+//       id: uuidv4(),
+//       title: metadata.title || "",
+//       description: metadata.description || "",
+//       category: metadata.category || "",
+//       tools: metadata.tools || "",
+//       tags: metadata.tags || "",
+//       fileurl: metadata.fileurl || "",
+//       url: `https://${bucketName}.s3.amazonaws.com/${fileName}`,
+//       shop: metadata.shop || "",
+//       sourcefile: metadata.sourcefile || "",
+//     };
+
+//     const dynamoDBParams = {
+//       TableName: "assets-table",
+//       Item: fileData,
+//     };
+
+//     const dynamoDBResponse = await dynamoDB.put(dynamoDBParams).promise();
+//     console.log("Metadata saved to DynamoDB:", dynamoDBResponse);
 
 //     return {
-//       statusCode: 200,
-//       body: JSON.stringify(searchResults),
+//       statusCode: 205,
+//       headers: {
+//         "Access-Control-Allow-Origin": "*",
+//         "Access-Control-Allow-Credentials": true,
+//       },
+//       body: JSON.stringify({
+//         message: "File uploaded successfully",
+//         status: "success",
+//       }),
 //     };
 //   } catch (error) {
 //     console.error(error);
 //     return {
 //       statusCode: 500,
-//       body: JSON.stringify({ error: "Internal server error" }),
+//       body: JSON.stringify({ message: "Failed to upload file" }),
+//     };
+//   }
+// };
+
+const { v4: uuidv4 } = require("uuid");
+// const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+module.exports.uploadFile = async (event, context) => {
+  try {
+    if (!event.body) {
+      throw new Error("Missing file content");
+    }
+
+    const fileContent = Buffer.from(event.body, "base64");
+    const fileName = event.queryStringParameters.filename || "defaultFilename";
+    const bucketName =
+      "my-serverless-app-dev-serverlessdeploymentbucket-ew2x60uk92zg";
+    const fileType = fileName.split(".")[1];
+
+    const params = {
+      Bucket: bucketName,
+      Key: fileName,
+      Body: fileContent,
+      ContentType: `application/${fileType}`,
+      ACL: "public-read",
+    };
+
+    const result = await s3.upload(params).promise();
+    console.log(`File uploaded successfully to ${result.Location}`);
+
+    const metadata = JSON.parse(event.body);
+
+    const categoryData = {
+      id: uuidv4(),
+      name: metadata.category || "",
+    };
+
+    const categoryParams = {
+      TableName: "new-category-table",
+      Item: categoryData,
+    };
+
+    const categoryResponse = await dynamoDB.put(categoryParams).promise();
+    console.log("Category data saved to DynamoDB:", categoryResponse);
+
+    const fileData = {
+      id: uuidv4(),
+      title: metadata.title || "",
+      description: metadata.description || "",
+      category: categoryData.id, // Save the category ID in the assets table
+      tools: metadata.tools || "",
+      tags: metadata.tags || "",
+      fileurl: metadata.fileurl || "",
+      url: `https://${bucketName}.s3.amazonaws.com/${fileName}`,
+      shop: metadata.shop || "",
+      sourcefile: metadata.sourcefile || "",
+    };
+
+    const assetParams = {
+      TableName: "assets-table",
+      Item: fileData,
+    };
+
+    const assetResponse = await dynamoDB.put(assetParams).promise();
+    console.log("Asset data saved to DynamoDB:", assetResponse);
+
+    return {
+      statusCode: 205,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify({
+        message: "File uploaded successfully",
+        status: "success",
+      }),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Failed to upload file" }),
+    };
+  }
+};
+
+
+
+// Create File
+module.exports.getFile = async (event, context) => {
+  try {
+    const fileId = event.queryStringParameters.id;
+
+    const dynamoDBParams = {
+      TableName: "assets-table",
+      Key: {
+        id: fileId,
+      },
+    };
+
+    const dynamoDBResponse = await dynamoDB.get(dynamoDBParams).promise();
+    const fileData = dynamoDBResponse.Item;
+
+    if (!fileData) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: "File not found" }),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify(fileData),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Failed to retrieve file" }),
+    };
+  }
+};
+
+// Update Assets
+
+module.exports.updateFile = async (event, context) => {
+  try {
+    if (!event.body) {
+      throw new Error("Missing update data");
+    }
+
+    // Parse the update data from the request body
+    const updateData = JSON.parse(event.body);
+
+    const fileId = updateData.id;
+    if (!fileId) {
+      throw new Error("Missing file ID");
+    }
+
+    const dynamoDBParams = {
+      TableName: "assets-table",
+      Key: { id: fileId },
+      UpdateExpression:
+        "set title = :title, description = :description, tools = :tools, tags = :tags",
+      ExpressionAttributeValues: {
+        ":title": updateData.title || "",
+        ":description": updateData.description || "",
+        ":tools": updateData.tools || "",
+        ":tags": updateData.tags || "",
+      },
+      ReturnValues: "ALL_NEW",
+    };
+
+    const dynamoDBResponse = await dynamoDB.update(dynamoDBParams).promise();
+    console.log("Metadata updated in DynamoDB:", dynamoDBResponse);
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify({
+        message: "File metadata updated successfully",
+        status: "success",
+      }),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Failed to update file metadata" }),
+    };
+  }
+};
+
+// deletefile
+
+module.exports.deleteFile = async (event, context) => {
+  try {
+    const fileId = event.queryStringParameters.id;
+
+    const dynamoDBParams = {
+      TableName: "assets-table",
+      Key: {
+        id: fileId,
+      },
+    };
+
+    const dynamoDBResponse = await dynamoDB.delete(dynamoDBParams).promise();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify({ message: "File deleted successfully" }),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Failed to delete file" }),
+    };
+  }
+};
+
+
+
+// category tables
+// const dynamoDb = new AWS.DynamoDB.DocumentClient();
+// const uuid = require("uuid");
+// module.exports.createCategory = async (event, context) => {
+//   const { name } = JSON.parse(event.body);
+
+//   const category = {
+//     id: uuid.v1(),
+//     name,
+//   };
+
+//   const params = {
+//     TableName: "new-category-table",
+//     Item: category,
+//   };
+
+//   try {
+//     await dynamoDb.put(params).promise();
+//     return {
+//       statusCode: 201,
+//       body: JSON.stringify(category),
+//     };
+//   } catch (error) {
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: "Could not create category" }),
 //     };
 //   }
 // };
 
 
+// get Category
+module.exports.getCategory = async (event, context) => {
+  const { id } = event.queryStringParameters;
+
+  const params = {
+    TableName: "new-category-table",
+    Key: {
+      id: id,
+    },
+  };
+
+  try {
+    const result = await dynamoDb.get(params).promise();
+
+    if (!result.Item) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ error: "Category not found" }),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Could not retrieve category" }),
+    };
+  }
+};
+
+
+
+// Tools Table
+// const uuid = require("uuid");
+// module.exports.createTools = async (event, context) => {
+//   const { name } = JSON.parse(event.body);
+
+//   const tools = {
+//     id: uuid.v1(),
+//     name,
+//   };
+
+//   const params = {
+//     TableName: "new-tools-table",
+//     Item: tools,
+//   };
+
+//   try {
+//     await dynamoDb.put(params).promise();
+//     return {
+//       statusCode: 201,
+//       body: JSON.stringify(tools),
+//     };
+//   } catch (error) {
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: "Could not create tools" }),
+//     };
+//   }
+// };
+
+// get Category
+module.exports.getTools = async (event, context) => {
+  const { id } = event.queryStringParameters;
+
+  const params = {
+    TableName: "new-tools-table",
+    Key: {
+      id: id,
+    },
+  };
+
+  try {
+    const result = await dynamoDb.get(params).promise();
+
+    if (!result.Item) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ error: "Tools not found" }),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Could not retrieve tools" }),
+    };
+  }
+};
+
+
+
+ //  Search Service
 
 module.exports.SearchService = async (event) => {
   try {
     console.log("Received event:", JSON.stringify(event));
 
     const { queryStringParameters } = event;
-    const { title, description, type, category_id, isPremium } =
-      queryStringParameters;
+    const { title, description, type, isPremium } = queryStringParameters;
 
     console.log("title:", title);
 
     const searchParams = {
       TableName: "assets-table",
       FilterExpression:
-        "contains(#title, :title) OR contains(#description, :description) OR #type = :type OR #category_id = :category_id OR #isPremium = :isPremium",
+        "contains(#title, :title) OR contains(#description, :description) OR #type = :type OR #isPremium = :isPremium",
       ExpressionAttributeNames: {
         "#title": "title",
         "#description": "description",
         "#type": "type",
-        "#category_id": "category_id",
         "#isPremium": "isPremium",
       },
       ExpressionAttributeValues: {
         ":title": title,
         ":description": description,
         ":type": type,
-        ":category_id": category_id,
         ":isPremium": isPremium,
       },
     };
